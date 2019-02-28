3e55265a97acbe2d7e9fbbfb36f0460d46b3ada8
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-28 00:46:29 -0600

    Attempting translate only solution

--- src/toolbox/components/sticky/base.ts
+++ src/toolbox/components/sticky/base.ts
@@ -1,25 +1,8 @@
 import {NumericRange} from "../../utils/math/numeric-range";
 import {getVisibleYPosition} from "../../utils/dom/position/vertical/get-visible-y-position";
 import {renderLoop} from "../../utils/render-loop";
-import {getVisibleDistanceFromRoot} from "../../utils/dom/position/get-visible-distance-from-root";
 import {Vector2d} from "../../utils/math/geometry/vector-2d";
-import {getVisibleDistanceBetweenElements} from "../../utils/dom/position/get-visible-distance-between-elements";
-import {Scroll} from "../../utils/cached-vectors/scroll";
-import {Dimensions2d} from "../../utils/math/geometry/dimensions-2d";
-
-/**
- * Positions the the element could be sticking to within the container.
- *
- * How the target element is positioned by sticky will change depending on
- * whether its container is showing its top, middle or bottom most prominently.
- *
- * @hidden
- */
-class ContainerPosition {
-  public static TOP: Symbol = Symbol('top');
-  public static MIDDLE: Symbol = Symbol('middle');
-  public static BOTTOM: Symbol = Symbol('bottom');
-}
+import {translate2dOnScrollLoop} from "../../utils/dom/position/translate-2d-on-scroll-loop";
 
 /**
  * Simulates `position: sticky` when native support isn't available due to
@@ -29,8 +12,7 @@ class Sticky {
   private readonly container_: HTMLElement;
   private readonly target_: HTMLElement;
   private destroyed_: boolean;
-  private lastPosition_: Symbol;
-  private lastWindowDimensions_: Dimensions2d;
+  private totalYAdded_: number;
 
   /**
    * @param target The Element to position as if it were "position: sticky"'d
@@ -43,9 +25,8 @@ class Sticky {
   constructor (target: HTMLElement, container: HTMLElement) {
     this.container_ = container;
     this.target_ = target;
-    this.lastPosition_ = null;
     this.destroyed_ = false;
-    this.lastWindowDimensions_ = Dimensions2d.fromInnerWindow();
+    this.totalYAdded_ = 0;
     this.init_();
   }
 
@@ -62,16 +43,6 @@ class Sticky {
     renderLoop.scrollMeasure(() => this.measure_());
   }
 
-  private getPosition_(shouldPin: boolean, yPosition: number): Symbol {
-    if (shouldPin) {
-      return ContainerPosition.MIDDLE;
-    } else if (yPosition < 0) {
-      return ContainerPosition.BOTTOM;
-    } else {
-      return ContainerPosition.TOP;
-    }
-  }
-
   // Split out so it can be run on initial load
   private measure_(): void {
     if (this.destroyed_) {
@@ -85,90 +56,15 @@ class Sticky {
       this.container_.offsetHeight -
       this.target_.offsetHeight -
       this.target_.offsetTop;
-    const shouldPin = new NumericRange(0, maxDistance).contains(-yPosition);
-    const position = this.getPosition_(shouldPin, yPosition);
-    const windowDimensions = Dimensions2d.fromInnerWindow();
-    const targetOffsetTop = this.target_.offsetTop;
 
-    // Ensure cached values are updated even if there's an early exit
-    // Will update before the next loop but not before the current loop is
-    // completed.
-    renderLoop.scrollPremeasure(() => {
-      this.lastPosition_ = position;
-      this.lastWindowDimensions_ = windowDimensions;
-    });
+    const rawChange = Math.abs(yPosition) - this.totalYAdded_;
+    const clampedChange =
+      new NumericRange(-this.totalYAdded_, maxDistance - this.totalYAdded_)
+        .clamp(rawChange);
 
-    // Skip duplicating work
-    if (
-      this.lastPosition_ === position &&
-      (
-        position === ContainerPosition.TOP ||
-        this.lastWindowDimensions_.equals(windowDimensions)
-      )
-    ) {
-      return;
-    }
+    translate2dOnScrollLoop(this.target_, new Vector2d(0, clampedChange));
 
-    // Determine if the target should stick
-    if (position === ContainerPosition.TOP) {
-      this.positionTop_();
-    }
-    else if (position === ContainerPosition.BOTTOM) {
-      this.positionBottom_(targetOffsetTop, maxDistance);
-    }
-    else if (position === ContainerPosition.MIDDLE) {
-      if (!this.lastWindowDimensions_.equals(windowDimensions)) {
-        this.positionTop_(); // Necessary thrashing. :(
-      }
-
-      const oldPositionFromWindow =
-        getVisibleDistanceFromRoot(this.target_);
-      const originalSizing = Dimensions2d.fromElementOffset(this.target_);
-      const desiredPositionFromWindow =
-        new Vector2d(oldPositionFromWindow.x, 0);
-
-      // Avoid thrashing the final update
-      renderLoop.scrollMutate(() => {
-        this.positionMiddle_(targetOffsetTop);
-        desiredPositionFromWindow.positionElementByTranslation(this.target_);
-        originalSizing.sizeElement(this.target_);
-      });
-    }
-  }
-
-  private positionTop_(): void {
-    renderLoop.scrollMutate(() => {
-      this.clearStyles_();
-    });
-  }
-
-  private positionMiddle_(targetOffsetTop: number): void {
-    this.clearStyles_();
-    this.target_.style.position = 'fixed';
-    this.target_.style.margin = '0';
-    this.target_.style.marginTop = `${targetOffsetTop}px`;
-    this.target_.style.top = '0';
-    this.target_.style.left = '0';
-  }
-
-  private positionBottom_(targetOffsetTop: number, maxDistance: number): void {
-    renderLoop.scrollMutate(() => {
-      this.clearStyles_();
-      this.target_.style.transform =
-        `translateY(${maxDistance - targetOffsetTop}px)`;
-      this.target_.style.marginTop = `${targetOffsetTop}px`;
-    });
-  }
-
-  private clearStyles_() {
-    this.target_.style.position = '';
-    this.target_.style.width = '';
-    this.target_.style.height = '';
-    this.target_.style.transform = '';
-    this.target_.style.margin = '';
-    this.target_.style.marginTop = '';
-    this.target_.style.top = '';
-    this.target_.style.left = '';
+    this.totalYAdded_ += clampedChange;
   }
 
   public destroy() {
