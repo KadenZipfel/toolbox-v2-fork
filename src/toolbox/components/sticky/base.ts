ea0078c9710c67ac7a95c359007bbf17cea2fc19
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-28 23:19:14 -0600

    Preserve original Sticky with Sticky2

--- src/toolbox/components/sticky/base.ts
+++ src/toolbox/components/sticky/base.ts
@@ -2,59 +2,6 @@ import {NumericRange} from "../../utils/math/numeric-range";
 import {getVisibleYPosition} from "../../utils/dom/position/vertical/get-visible-y-position";
 import {renderLoop} from "../../utils/render-loop";
 import {getOffsetFromAncestor} from "../../utils/dom/position/get-offset-from-ancestor";
-import {Vector2d} from "../../utils/math/geometry/vector-2d";
-import {Dimensions2d} from "../../utils/math/geometry/dimensions-2d";
-import {getVisibleDistanceBetweenElements} from "../../utils/dom/position/get-visible-distance-between-elements";
-import {getVisibleDistanceFromRoot} from "../../utils/dom/position/get-visible-distance-from-root";
-import {getCommonOffsetAncestor} from "../../utils/dom/position/get-common-offset-ancestor";
-import {getCommonPositionedParentElement} from "../../utils/dom/position/get-common-positioned-parent-element";
-
-/**
- * TODO(Angus)
- *
- * Use the target's computed style and apply it as inline styles to a div.
- * Stick that div into the container in place of the target when the target is fixed.
- * Remove that div from the container when the target stops being fixed.
- * This should keep the container's sizing constant.
- *
- * Translate the element based on the
- *
- * .... screw all that.
- *
- *
- * Here's the new rules:
- *
- * Container size should be independent of the Sticky element.
- * Which is to say that if the sticky element can change the computed style of
- * its container, this component WILL NOT WORK.
- *
- * On load add a fixed div that per-frame matches the dimensions of the
- * given container. Fixed div will stay fixed at top: 0 and left: 0 but will
- * apply a translateX to match the left edge of the container.
- *
- * When in the MIDDLE position, append the target to the fixed div.
- * In other positions append the target to the original parent.
- *
- * Remove the need to specify the container, assume it is always the offsetParent
- * of the target.
- *
- * WON'T WORK. STICKY KEEPS OTHER ELEMENTS IN FLOW, SO WE HAVE TO KEEP THEM IN FLOW.
- *
- *
- * NEW NEW PLAN: !!!!!
- *
- * Clone target. Place clone in-place of target with visibility: hidden
- *
- * Keep clone inner-html up to date per frame.
- *
- * Position target absolutely. Use left-right to keep it on top of clone.
- *
- * When in MIDDLE position it fixed and match the width/height to the clone.
- *
- * Should prevent emergency thrashing and provide a basis to pull MIDDLE values
- * from.
- *
- */
 
 /**
  * Positions the the element could be sticking to within the container.
@@ -77,7 +24,6 @@ class ContainerPosition {
 class Sticky {
   private readonly container_: HTMLElement;
   private readonly target_: HTMLElement;
-  private readonly clone_: HTMLElement;
   private destroyed_: boolean;
   private lastPosition_: Symbol;
 
@@ -94,24 +40,10 @@ class Sticky {
     this.target_ = target;
     this.lastPosition_ = null;
     this.destroyed_ = false;
-    this.clone_ = document.createElement(target.tagName);
     this.init_();
   }
 
   private init_(): void {
-    this.clone_.innerHTML = this.target_.innerHTML;
-    this.clone_.style.visibility = 'hidden';
-    Array.from(this.target_.classList)
-      .forEach((className) => this.clone_.classList.add(className));
-    this.container_.appendChild(this.clone_);
-    this.target_.style.position = 'absolute';
-    this.target_.style.top = '0';
-    this.target_.style.left = '0';
-    this.target_.style.width = '';
-    this.target_.style.height = '';
-    this.target_.style.margin = '0';
-    this.target_.style.padding = '0';
-
     this.measure_();
     this.renderLoop_();
   }
@@ -121,13 +53,10 @@ class Sticky {
       return;
     }
 
-    renderLoop.scrollMeasure(() => {
-      renderLoop.scrollCleanup(() => this.renderLoop_());
-      this.measure_()
-    });
+    renderLoop.scrollMeasure(() => this.measure_());
   }
 
-  private static getPosition_(shouldPin: boolean, yPosition: number): Symbol {
+  private getPosition_(shouldPin: boolean, yPosition: number): Symbol {
     if (shouldPin) {
       return ContainerPosition.MIDDLE;
     } else if (yPosition < 0) {
@@ -139,21 +68,21 @@ class Sticky {
 
   // Split out so it can be run on initial load
   private measure_(): void {
+    if (this.destroyed_) {
+      return;
+    }
+
+    renderLoop.scrollCleanup(() => this.renderLoop_());
+
     const yPosition: number = getVisibleYPosition(this.container_);
     const maxDistance: number =
       this.container_.offsetHeight -
-      this.clone_.offsetHeight -
-      this.clone_.offsetTop;
+      this.target_.offsetHeight -
+      this.target_.offsetTop;
     const shouldPin = new NumericRange(0, maxDistance).contains(-yPosition);
-    const position = Sticky.getPosition_(shouldPin, yPosition);
-
-    const commonFrame =
-      getCommonPositionedParentElement(this.clone_, this.target_);
-    const cloneDistanceFromFrame =
-      getVisibleDistanceBetweenElements(this.clone_, commonFrame);
-    const cloneDistanceFromRoot = getVisibleDistanceFromRoot(this.clone_);
-    const cloneStyle = window.getComputedStyle(this.clone_);
-
+    const position = this.getPosition_(shouldPin, yPosition);
+    const containerXOffset: number =
+      getOffsetFromAncestor(this.container_, null).x;
 
     // Skip duplicating work
     if (this.lastPosition_ === position) {
@@ -161,39 +90,34 @@ class Sticky {
     }
 
     renderLoop.scrollMutate(() => {
-      this.applyCloneStylesToTarget_(cloneStyle);
-
       // Determine if the target should stick
       if (position === ContainerPosition.TOP) {
-        this.target_.style.position = 'absolute';
-        cloneDistanceFromFrame.positionElementByTranslation(this.target_);
+        this.positionTop_();
       }
       else if (position === ContainerPosition.MIDDLE) {
-        this.target_.style.position = 'fixed';
-        new Vector2d(cloneDistanceFromRoot.x, cloneDistanceFromFrame.y)
-          .positionElementByTranslation(this.target_);
+        this.positionMiddle_(containerXOffset);
       }
       else if (position === ContainerPosition.BOTTOM) {
-        this.target_.style.position = 'absolute';
-        cloneDistanceFromFrame
-          .add(new Vector2d(0, maxDistance))
-          .positionElementByTranslation(this.target_);
+        this.positionBottom_(maxDistance);
       }
 
       this.lastPosition_ = position;
     });
   }
 
-  private applyCloneStylesToTarget_(cloneStyles: CSSStyleDeclaration): void {
-    this.target_.style.margin = '0';
-    this.target_.style.top = '0';
-    this.target_.style.bottom = '0';
-    this.target_.style.left = '0';
-    this.target_.style.right = '0';
-    this.target_.style.padding = cloneStyles.padding;
-    this.target_.style.width = cloneStyles.width;
-    this.target_.style.height = cloneStyles.height;
-    this.target_.style.border = cloneStyles.border;
+  private positionTop_(): void {
+    this.target_.style.position = '';
+    this.target_.style.transform = '';
+  }
+
+  private positionMiddle_(containerXOffset: number): void {
+    this.target_.style.position = 'fixed';
+    this.target_.style.transform = `translateX(${containerXOffset}px)`;
+  }
+
+  private positionBottom_(maxDistance: number): void {
+    this.target_.style.position = 'absolute';
+    this.target_.style.transform = `translateY(${maxDistance}px)`;
   }
 
   public destroy() {
