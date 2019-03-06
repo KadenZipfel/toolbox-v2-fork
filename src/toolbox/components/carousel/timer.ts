8e9c8b172ece75e705fb84c58d4765046ac07326
Angus MacDonald <angusamacdonald@hotmail.com>
2019-03-06 13:15:51 -0600

    Add pause/play/stop to CarouselTimer

--- src/toolbox/components/carousel/timer.ts
+++ src/toolbox/components/carousel/timer.ts
@@ -7,11 +7,16 @@ class CarouselTimer {
   private readonly interval_: number;
   private readonly carousel_: Carousel;
   private lastActionTime_: number;
+  private destroyed_: boolean;
+  private paused_: boolean;
+  private pausedOffset_: number;
 
   constructor(carousel: Carousel, interval: number = DEFAULT_INTERVAL) {
     this.carousel_ = carousel;
     this.interval_ = interval;
     this.lastActionTime_ = new Date().valueOf();
+    this.paused_ = false;
+    this.pausedOffset_ = 0;
     this.init_();
   }
 
@@ -20,6 +25,10 @@ class CarouselTimer {
   }
 
   private startTimeout_(): void {
+    if (this.paused_ || this.destroyed_) {
+      return;
+    }
+
     renderLoop.measure(() => {
       if (new Date().valueOf() > +this.lastActionTime_ + this.interval_) {
         this.carousel_.next();
@@ -29,6 +38,34 @@ class CarouselTimer {
       }
       renderLoop.mutate(() => this.startTimeout_());
     });
+  }
+
+  public pause() {
+    if (this.paused_) {
+      return;
+    }
+
+    this.paused_ = true;
+    this.pausedOffset_ = new Date().valueOf() - this.lastActionTime_;
+  }
+
+  public stop() {
+    this.pause();
+    this.pausedOffset_ = 0;
+  }
+
+  public play() {
+    if (!this.paused_) {
+      return; // Makes multiple calls when playing safe
+    }
+    this.lastActionTime_ = new Date().valueOf() - this.pausedOffset_;
+    this.paused_ = false;
+    this.pausedOffset_ = 0;
+    this.startTimeout_();
+  }
+
+  public destroy() {
+    this.destroyed_ = true;
   }
 }
 
