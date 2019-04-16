f75e1eb7bf97312d00a4a9e3441a05f3cac0cc02
Angus MacDonald <angusamacdonald@hotmail.com>
2019-04-16 11:51:00 -0600

    Bugfix loopSlice

--- src/toolbox/utils/array/loop-slice.ts
+++ src/toolbox/utils/array/loop-slice.ts
@@ -1,15 +1,23 @@
 import {getSign} from "../math/get-sign";
 
+function wrapIndex(index: number, length: number): number {
+  if (index < 0) {
+    return length + (index % length);
+  } else {
+    return index % length;
+  }
+}
+
 function loopSlice<T>(
   values: T[], startIndex: number, endIndex: number, direction: number
 ): T[] {
   const result: T[] = [];
   const length = values.length;
   const increment = getSign(direction);
-  let index = startIndex;
+  let index = wrapIndex(startIndex, length);
   while (index !== endIndex) {
     result.push(values[index]);
-    index = (index + increment) % length;
+    index = wrapIndex(index + increment, length);
   }
 
   return result;
